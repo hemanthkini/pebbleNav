65f90f9515c10dbe6654752161fd93a9ff1aab11
tconley1428 <tconley1428@gmail.com>
2013-09-14 19:44:34 -0400

    JSON

--- src/com/example/pebblenav/Direction.java
+++ src/com/example/pebblenav/Direction.java
@@ -1,15 +1,72 @@
 package com.example.pebblenav;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.json.JSONObject;
+
 public class Direction {
 
 	public String instructions;
+
+	//Possibly null
 	public String maneuver;
-	public String distance;
-	
-	public Direction(String instructions, String maneuver, String distance)
+
+
+	public String distancetext;
+	public double distanceval;
+
+	public double startlat;
+	public double startlong;
+	public double endlat;
+	public double endlong;
+
+	public Direction(JSONObject s)
 	{
-		this.instructions=instructions;
-		this.maneuver = maneuver;
-		this.distance = distance;
+		try{
+			instructions = s.getString("html_instructions");
+			distanceval = s.getJSONObject("distance").getDouble("value");
+			distancetext = s.getJSONObject("distance").getString("text");
+
+			String man="";
+			try{
+				man = s.getString("maneuver");
+			}
+			catch(Exception ex){}
+			maneuver=man;
+			startlat=s.getJSONObject("start_location").getDouble("lat");
+			startlong=s.getJSONObject("start_location").getDouble("lng");
+			endlat=s.getJSONObject("end_location").getDouble("lat");
+			endlong=s.getJSONObject("end_location").getDouble("lng");
+
+
+			Pattern P = Pattern.compile("(.*)(<div[^<|>]*>)(.*)");
+			Matcher M = P.matcher(instructions);
+
+			while(M.matches())
+			{
+				instructions=M.group(1)+"NEWLINE"+M.group(3);
+				M=P.matcher(instructions);
+			}
+
+			Pattern P2 = Pattern.compile("(.*)(<[^<|>]*>)(.*)");
+			Matcher M2 = P2.matcher(instructions);
+
+			while(M2.matches())
+			{
+				instructions=M2.group(1)+M2.group(3);
+				M2=P2.matcher(instructions);
+			}
+			instructions = instructions.replaceAll("NEWLINE", "\n");
+		}
+		catch(Exception ex)
+		{
+			System.out.println(ex.getStackTrace());
+		}
+	}
+
+	public String toString()
+	{
+		return instructions;
 	}
 }
--- src/com/example/pebblenav/JsonDirectionParser.java
+++ src/com/example/pebblenav/JsonDirectionParser.java
@@ -3,6 +3,7 @@ package com.example.pebblenav;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -21,26 +22,27 @@ public class JsonDirectionParser {
 	{
 		ArrayList<Direction> parsedDirections = new ArrayList<Direction>();
 
-		JSONObject jobject = new JSONObject(json);
-		Iterator iter = jobject.keys();
-		while(iter.hasNext())
-			Log.d("JSON",iter.next().toString());
-		
-		/*JSONArray steps = jobject.getJSONArray("steps");
-		for (int i=0; i < steps.length(); i++)
+		JSONObject root = new JSONObject(json);
+		JSONArray routes = root.getJSONArray("routes");
+		if(routes.length()>0)
 		{
-		    try {
-		        JSONObject oneObject = steps.getJSONObject(i);
-		        // Pulling items from the array
-		        String distance = oneObject.getString("distance");
-		        String instructions = oneObject.getString("html_instructions");
-		        String maneuver = oneObject.getString("maneuver");
-		        Direction D = new Direction(distance,instructions,maneuver);
-		        parsedDirections.add(D);
-		    } catch (JSONException e) {
-		        // Oops
-		    }
-		}*/
+			JSONObject route = routes.getJSONObject(0);
+			JSONArray legs = route.getJSONArray("legs");
+			int count = legs.length();
+			if(count==1)
+			{
+				JSONArray steps = legs.getJSONObject(0).getJSONArray("steps");
+				System.out.println(steps);
+				for(int i =0; i<steps.length();i++)
+				{
+					JSONObject s = steps.getJSONObject(i);
+
+					Direction D = new Direction(steps.getJSONObject(i));
+					parsedDirections.add(D);
+				}
+			}
+			
+		}
 		
 		return parsedDirections;
 	}
