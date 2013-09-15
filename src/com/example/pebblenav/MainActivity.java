deb175aa9570868d0c09a581caffa8ac6316f678
tconley1428 <tconley1428@gmail.com>
2013-09-15 01:46:29 -0400

    distance refactor

--- src/com/example/pebblenav/MainActivity.java
+++ src/com/example/pebblenav/MainActivity.java
@@ -8,6 +8,8 @@ import java.util.concurrent.TimeUnit;
 
 import org.json.JSONException;
 
+import com.example.pebblenav.util.DistanceUtils;
+
 import android.app.Activity;
 import android.graphics.Typeface;
 import android.os.Bundle;
@@ -64,7 +66,7 @@ public class MainActivity extends Activity  implements Runnable{
 	public void recieveNewCoord(double latitude, double longitude){
 		try{
 			System.out.println("a");
-			final double dist = distance(latitude,longitude,directions.get(0).endlat,directions.get(0).endlong,'K')*1000;
+			final double dist = DistanceUtils.distance(latitude,longitude,directions.get(0).endlat,directions.get(0).endlong,'K')*1000;
 
 			runOnUiThread(new Runnable(){
 
@@ -122,47 +124,6 @@ public class MainActivity extends Activity  implements Runnable{
 
 	}
 
-	private double minimum_distance(vec2 v, vec2 w, vec2 p) {
-		  // Return minimum distance between line segment vw and point p
-		  double l2 = w.sub(v).mag2();  // i.e. |w-v|^2 -  avoid a sqrt
-		  if (l2 == 0.0) return v.sub(p).magnitude();   // v == w case
-		  // Consider the line extending the segment, parameterized as v + t (w - v).
-		  // We find projection of point p onto the line. 
-		  // It falls where t = [(p-v) . (w-v)] / |w-v|^2
-		  double t = p.sub(v).dot(w.sub(v)) / l2;
-		  if (t < 0.0) return v.sub(p).magnitude();     // Beyond the 'v' end of the segment
-		  else if (t > 1.0) return w.sub(p).magnitude(); // Beyond the 'w' end of the segment
-		  vec2 projection = v.add((w.sub(v)).scale(t));  // Projection falls on the segment
-		  return projection.sub(p).magnitude();
-		}
-	
-    private double distance(double lat1, double lon1, double lat2, double lon2, char unit) {
-        double theta = lon1 - lon2;
-        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
-        dist = Math.acos(dist);
-        dist = rad2deg(dist);
-        dist = dist * 60 * 1.1515;
-        if (unit == 'K') {
-          dist = dist * 1.609344;
-        } else if (unit == 'N') {
-          dist = dist * 0.8684;
-          }
-        return (dist);
-      }
-
-      /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-      /*::  This function converts decimal degrees to radians             :*/
-      /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-      private double deg2rad(double deg) {
-        return (deg * Math.PI / 180.0);
-      }
-
-      /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-      /*::  This function converts radians to decimal degrees             :*/
-      /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-      private double rad2deg(double rad) {
-        return (rad * 180.0 / Math.PI);
-      }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -185,7 +146,7 @@ public class MainActivity extends Activity  implements Runnable{
 					vec2 B = new vec2(theDir.endlat,theDir.endlong);
 					vec2 C = new vec2(tracker.getLatitude(), tracker.getLongitude());
 					
-					double minDist = minimum_distance(A,B,C);
+					double minDist = DistanceUtils.minimum_distance(A,B,C);
 					
 					((TextView)(findViewById(R.id.distOffPath))).setText(minDist+" off path");
 					
