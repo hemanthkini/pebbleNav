ed4fa8ef1a8246f2f4598de6fc7014fb0811d9a8
tconley1428 <tconley1428@gmail.com>
2013-09-15 02:05:40 -0400

    no more line splitting

---
+++ src/com/example/pebblenav/util/LatLng.java
@@ -0,0 +1,13 @@
+package com.example.pebblenav.util;
+
+public class LatLng {
+
+	public double latitude;
+	public double longitude;
+	
+	public LatLng(double lat, double lng)
+	{
+		latitude = lat;
+		longitude = lng;
+	}
+}
